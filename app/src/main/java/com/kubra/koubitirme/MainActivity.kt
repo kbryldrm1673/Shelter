869059fda2a6d51d350f964859b651a5cecb236a
kbryldrm1673 <kubrayildirim1673@gmail.com>
2020-05-11 22:10:23 +0300

    Splash screen and Home screen page desing

--- app/src/main/java/com/kubra/koubitirme/MainActivity.kt
+++ app/src/main/java/com/kubra/koubitirme/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.kubra.koubitirme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var email: EditText? = null
+    private var password: EditText? = null
+    private var btn1: Button? = null
+    private var btn2: ImageButton?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        email = findViewById(R.id.editText)
+        password = findViewById(R.id.editText2)
+        btn1 = findViewById(R.id.button)
+        btn2 = findViewById(R.id.register_button)
+
+
+        register_button.setOnClickListener(View.OnClickListener { RegisterActivity() })
+
+
+
     }
+
 }
