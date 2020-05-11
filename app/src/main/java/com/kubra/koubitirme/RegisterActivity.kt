d80fabcf5c2b4ae128163964ca1df089f9e0a188
kbryldrm1673 <kubrayildirim1673@gmail.com>
2020-05-11 22:12:27 +0300

    Splash screen and Home screen page desing

---
+++ app/src/main/java/com/kubra/koubitirme/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.kubra.koubitirme
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity:AppCompatActivity() {
+
+    private var singUp: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        singUp = findViewById(R.id.button1)
+    }
+}
